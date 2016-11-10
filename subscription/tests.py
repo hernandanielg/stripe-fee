52217c5790a7c150cb9719788ca9e317f140a98c
Hernan Garcia <hernan.garcia@imaginamos.com>
2016-11-10 03:30:25 -0500

    added test for is_registered unit

--- subscription/tests.py
+++ subscription/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import User
 
 # Create your tests here.
+class UserTestCase(TestCase):
+
+    user = User()
+
+    def setUp(self):
+        self.user = User.objects.create(name="joe",email="joe@mail.com")
+
+    def test_user_is_registered(self):
+        self.assertIs(User.is_registered(self.user.email),True)
